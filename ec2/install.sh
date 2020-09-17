dba1ac8ef813663118785b64f074426c8ae581b9
Egor Zuev <zyev.egor@rambler.ru>
2020-09-17 21:11:43 +0300

    *ec2

--- ec2/install.sh
+++ ec2/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-sudo yum install -y nodejs git
+sudo apt-get update && sudo apt-get install -y nodejs npm git
 git clone https://github.com/ega-forever/akvelon-cloud-aws.git akvelon && \
   cd akvelon/ec2/app && npm install && \
   npm install -g pm2 && pm2 start index.js && \
