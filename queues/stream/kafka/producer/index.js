831510bbe64d8fdb90fe98beec508fec25edf940
zuevegor <zyev.egor@gmail.com>
2021-12-20 10:26:05 +0300

    *updated kafka demo

--- queues/stream/kafka/producer/index.js
+++ queues/stream/kafka/producer/index.js
@@ -1,45 +1,56 @@
 const config = require('../config'),
-  {Kafka} = require('kafkajs');
+    {Kafka} = require('kafkajs');
 
 const init = async () => {
 
-  const options = {
-    clientId: 'my-app',
-    brokers: config.connection.hosts.split(',')
-  };
-
-  if (config.connection.username && config.connection.password) {
-    Object.assign(options, {
-      sasl: {
-        username: config.connection.username,
-        password: config.connection.password,
-        mechanism: config.connection.authType
-      }
-    })
-  }
-
-  const kafka = new Kafka(options);
-
-
-  const producer = kafka.producer();
-
-  await producer.connect();
-
-
-  setInterval(async () => {
-
-    const message = {
-      key: 'timestamp',
-      value: Date.now().toString(),
-      partition: 0
+    const options = {
+        clientId: 'my-app',
+        brokers: config.connection.hosts.split(',')
     };
 
-    await producer.send({
-      topic: 'topic1',
-      messages: [message]
-    });
+    if (config.connection.username && config.connection.password) {
+        Object.assign(options, {
+            sasl: {
+                username: config.connection.username,
+                password: config.connection.password,
+                mechanism: config.connection.authType
+            }
+        })
+    }
 
-  }, 3000);
+    const kafka = new Kafka(options);
+    const admin = kafka.admin()
+    await admin.connect()
+
+    await admin.createTopics({
+        validateOnly: false,
+        waitForLeaders: true,
+        topics: [{
+            topic: 'topic1',
+            numPartitions: 3
+        }],
+    })
+    await admin.disconnect()
+
+
+    const producer = kafka.producer();
+    await producer.connect();
+
+
+    setInterval(async () => {
+
+        const message = {
+            keyTime: `timestamp_${Date.now()}`,
+            value: Date.now().toString(),
+            // partition: 0
+        };
+
+        await producer.send({
+            topic: 'topic1',
+            messages: [message]
+        });
+
+    }, 1000);
 
 
 };
